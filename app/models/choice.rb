bac05c403aeca358c3fa64f0435d07a0ba5bd6a4
Sihong31 <sihongspring2011@gmail.com>
2015-06-19 18:55:01 -0400

    associations all work

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,2 +1,4 @@
 class Choice < ActiveRecord::Base
+  belongs_to :question
+  has_many :users, through: :selections, foreign_key: :taker_id
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,6 @@
 class Survey < ActiveRecord::Base
+  has_many :completedsurveys
+  has_many :users, through: :completedsurveys, foreign_key: :taker_id
+  has_many :questions
+  belongs_to :creator, class_name: "User"
 end
