6470181b7a430c138a9577bd98a122ee89bcf55a
julia-castro <julia.castro@outlook.com>
2015-06-20 16:00:29 -0400

    first iteration of index and show views

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,26 @@
 class SurveysController < ApplicationController
+
+  def index
+    @surveys = Survey.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+    @survey = Survey.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 20150619212657) do
   end
 
   create_table "surveys", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
+    t.string   "title",       null: false
+    t.text     "description", null: false
     t.integer  "creator_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
