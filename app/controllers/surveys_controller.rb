4511ef96d594bba6745673e792964c2139f912f9
julia-castro <julia.castro@outlook.com>
2015-06-20 16:22:02 -0400

    first iteration create new survey view

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,9 +5,16 @@ class SurveysController < ApplicationController
   end
 
   def create
+    @survey = Survey.new(survey_params)
+    if @survey.save
+      redirect_to surveys_path
+    else
+      render :new
+    end
   end
 
   def new
+    @survey = Survey.new
   end
 
   def edit
@@ -21,6 +28,12 @@ class SurveysController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:title, :description)
   end
 
 end
