b2fc2188a4ae7f4b61e795601d98af8cfb5ac510
julia-castro <julia.castro@outlook.com>
2015-06-20 18:26:04 -0400

    UPDATE

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :choices
+
+  validates :content, presence: true
+
 end
--- db/migrate/20150619212505_create_surveys.rb
+++ db/migrate/20150619212505_create_surveys.rb
@@ -1,8 +1,8 @@
 class CreateSurveys < ActiveRecord::Migration
   def change
     create_table :surveys do |t|
-      t.string :title
-      t.text :description
+      t.string :title null: false
+      t.text :description null: false
       t.references :creator
 
       t.timestamps null: false
