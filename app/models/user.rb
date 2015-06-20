050b33dc8a354f05070313ad15e5d731ab204098
Sihong31 <sihongspring2011@gmail.com>
2015-06-20 14:44:10 -0400

    added sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+require "bcrypt"
+
 class User < ActiveRecord::Base
+ has_secure_password
  has_many :surveys, foreign_key: :creator_id
  has_many :completedsurveys, foreign_key: :taker_id
  has_many :selections, foreign_key: :taker_id
